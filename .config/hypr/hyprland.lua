edc9d19fee125cf862bdf6a5e8e86fec76838a2f
JxstPaul <124691605+JxstPaul@users.noreply.github.com>
2026-06-23 19:41:29 +0200

    Create hyprland.lua

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,45 @@
+--- MONITORS ----
+require("monitors")
+
+---- AUTOSTART ----
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("hyprpaper")
+    hl.exec_cmd("waybar")
+end)
+
+---- ENVIRONMENT VARIABLES ----
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+----- PERMISSIONS -----
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+
+require("style") -- Modifies the look and feel
+require("input") -- Sets keybinds & input settings
+
+---- WINDOWS AND WORKSPACES ----
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+local suppressMaximizeRule = hl.window_rule({
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+
+    suppress_event = "maximize"
+})
+
+hl.window_rule({
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true
+})
+
+suppressMaximizeRule:set_enabled(true)
