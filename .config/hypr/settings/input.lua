30c102ad4fef390e9763579f213d45b854029557
JxstPaul <124691605+JxstPaul@users.noreply.github.com>
2026-06-26 21:58:03 +0200

    Update input.lua

--- .config/hypr/settings/input.lua
+++ .config/hypr/settings/input.lua
@@ -16,7 +16,6 @@ hl.bind(mod .. " + ESCAPE", hl.dsp.exit())
 hl.bind("PRINT", hl.dsp.exec_cmd('grim -g "$(slurp)" - | wl-copy'))
 hl.bind("SHIFT + PRINT", hl.dsp.exec_cmd('grim -g "$(slurp) ~/Pictures/Screenshots'))
 
--- Window Controls
 hl.bind(mod .. " + Q", hl.dsp.window.close())
 hl.bind("F11", hl.dsp.window.fullscreen({ "maximized", toggle }))
 
@@ -26,19 +25,15 @@ hl.bind(mod .. " + RIGHT", hl.dsp.window.swap({ direction = "right" }))
 hl.bind(mod .. " + UP",    hl.dsp.window.swap({ direction = "up" }))
 hl.bind(mod .. " + DOWN",  hl.dsp.window.swap({ direction = "down" }))
 
--- App Launchers
-hl.bind(mod .. " + R", hl.dsp.exec_cmd(menu))
-hl.bind(mod .. " + T", hl.dsp.exec_cmd(terminal))
+hl.bind(mod .. " + SPACE", hl.dsp.exec_cmd(menu))
+hl.bind(mod .. " + R", hl.dsp.exec_cmd(terminal))
 
--- Audio Controls
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repea>
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true, repeating >
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true, repeating = true })
 hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true })
 
--- Microphone Controls
 hl.bind(mod .. " + M", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
 
--- Workspace Controls
 for i = 1, 4 do
     hl.bind(mod .. " + " .. i, hl.dsp.focus({ workspace = i }))
 end
